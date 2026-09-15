4792a303d7a76be0df2a25b0df3f03f03e90ddbc
Aleksandra Tsimentarova <cimentarowa@gmail.com>
2026-09-15 17:35:02 +0300

    style: Add grid layout

--- src/app/(marketing)/_components/about-section/index.tsx
+++ src/app/(marketing)/_components/about-section/index.tsx
@@ -5,16 +5,21 @@ import Container from '@/src/components/(layout)/container'
 
 const AboutSection = () => {
     return (
-        <div className="h-screen flex items-center justify-center mx-auto">
-            <Container className="">
-                <div className="flex flex-col items-start justify-start text-start">
-                    <div className="flex flex-col xl:flex-row gap-10 xl:gap-42">
-                        <div className="flex items-start shrink-0 gap-2">
-                            <p className="eyebrow-small uppercase">Chapter 1.1 About me</p>
+        <div className="min-h-screen mx-auto">
+            <Container>
+                <div className="grid-vars">
+                    <div className="grid md:grid-4 items-center min-h-screen">
+                        <div className="col-span-1">
+                            <div className="flex items-start shrink-0 gap-2">
+                                <p className="eyebrow-small uppercase">
+                                    Chapter 1.1 About me
+                                </p>
+                            </div>
                         </div>
-                        <div className="">
+
+                        <div className="col-span-3">
                             <TextAnimation>
-                                <h2 className="headline-h1 font-normal">
+                                <h2 className="headline-h2 font-normal">
                                     I’m a frontend developer crafting thoughtful digital experiences that connect design, technology and interaction. I help ambitious teams turn ideas into fast, polished interfaces.
                                 </h2>
                             </TextAnimation>
